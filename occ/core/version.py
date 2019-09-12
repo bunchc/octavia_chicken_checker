4e86b5271121afd8cf1be3780d3e40209cdf4614
Cody Bunch <bunchc@gmail.com>
2019-09-12 14:51:30 -0500

    version update

--- occ/core/version.py
+++ occ/core/version.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 
 
-VERSION = (0, 0, 1, 'alpha', 0)
+VERSION = (0, 0, 2, 'alpha', 0)
 
 def get_version(version=VERSION):  # pragma: nocover
     "Returns a PEP 386-compliant version number from VERSION."
